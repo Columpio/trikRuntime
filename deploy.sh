3fb05b5596518694ba1c0ac5959c02073bdbbf76
Yurii Kostyukov <columpio@inbox.ru>
2017-06-21 17:46:38 +0300

    deploy - no asking

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ else
 fi
 
 ssh root@$ip 'chmod -x /etc/trik/trikGui.sh && killall trikGui'
-rsync -avz -e ssh . root@$ip:/home/root/trik
+rsync -avz -e 'ssh -o StrictHostKeyChecking=no' . root@$ip:/home/root/trik
 ssh root@$ip 'chmod +x /etc/trik/trikGui.sh && kill -hup 1'
